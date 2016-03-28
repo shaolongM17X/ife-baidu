50e99ab713f281783dd67073614b27709ed27688
shaolong <shaolonglin09@gmail.com>
2016-03-28 01:55:34 -0400

    ready to implement in-order traverse

--- level2/task22/app.js
+++ level2/task22/app.js
@@ -10,7 +10,13 @@ var controller = {
 		if (inputIsValid(value)) {
 			modal.binaryTree.add(value);
 		}
-	}
+	},
+	getArrayRepresentation: function() {
+		return modal.binaryTree.toArray();
+	},
+	getHeight: function() {
+		return modal.binaryTree.heightOfTree();
+	},
 };
 
 var view = {
@@ -27,7 +33,28 @@ var view = {
 	},
 
 	render: function() {
-
+		var arr = controller.getArrayRepresentation();
+		var htmlArr = arr.map(function(value, pos) {
+			var power = Math.floor(Math.log2(pos + 1));
+			var width = 100 / (Math.pow(2,power));
+			if (value == '#') {
+				return '<li style="width:'+ width + '%"></li>';
+			}
+			return '<li style="width:'+ width + '%"><button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored" id="value' + value+ '">' + value+'</button></li>';
+		});
+		console.log(htmlArr);
+		var height = controller.getHeight();
+		$tree = document.getElementById('tree');
+		$tree.innerHTML = '';
+		for (var i = 1; i <= Math.pow(2, height - 1); i = 2*i) {
+			$tree.innerHTML = $tree.innerHTML + '<ul id="containerFor' + i + '"></ul>';
+			$container = document.getElementById("containerFor"+i);
+			var htmlToAppend = '';
+			for (var j = i-1; j <= 2*i - 2; j++) {
+				htmlToAppend += htmlArr[j];
+			}
+			$container.innerHTML = htmlToAppend;
+		}
 	}
 };
 

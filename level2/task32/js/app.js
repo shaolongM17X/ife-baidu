b298022e0527849b1588689354c20b295031b333
shaolongm17x <shaolonglin09@gmail.com>
2016-04-03 22:12:21 -0400

    done

--- level2/task32/js/app.js
+++ level2/task32/js/app.js
@@ -12,7 +12,7 @@ function getRequirementFromUser() {
             prepareFormOfStyle('material');
             componentHandler.upgradeDom();
         } else {
-            prepareFormOfStyle('bootstrap');
+            prepareFormOfStyle('nothing');
             componentHandler.upgradeDom();
         }
 
@@ -34,6 +34,22 @@ function prepareFormOfStyle(style) {
     if (userWants($("#phone-input"))) {
         addInputField(style, $generatedForm, 'phone');
     }
+    addSubmitButton($generatedForm);
+}
+
+function addSubmitButton($generatedForm) {
+    $generatedForm.append('<button class="mdl-button mdl-js-button mdl-button--primary" id="submit-btn">Submit</button>');
+    $("#submit-btn").click(function(e) {
+        e.preventDefault();
+        var $validators = $("span[id^=msgFor]");
+        for (var i = 0; i< $validators.length; i++) {
+            if ($validators[i].style.color == 'red') {
+                alert("The form is not ready..");
+                return;
+            }
+        }
+        alert("The form is ready.");
+    });
 }
 
 function FormList(name, type, func) {
@@ -43,28 +59,38 @@ function FormList(name, type, func) {
 }
 
 function addInputField(style, $generatedForm, name, validator) {
-    var newInputField = new FormList(name, 'text', prepareValidators(name));
+    var type = name == 'password'? 'password' : 'text';
+    var newInputField = new FormList(name, type, prepareValidators(name));
     prepareDOMnodeFor(style, $generatedForm, newInputField);
 }
 
 function prepareValidators(name) {
     var validator = {};
+    validator.msg = new Map();
+    validator.msg.set('success', "This " + name +" seems valid.");
     if (name == 'name') {
-        validator.msg = new Map();
         validator.msg.set('invalid', "Your name is invalid since it contains special characters.")
-        validator.msg.set('success', "This name seems valid.");
         validator.check = function(val) {
             return /^[^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\d]+$/.test(val);
         }
     }
     if (name == 'password') {
-
+        validator.msg.set('invalid', "Your password should contain special characters, numbers and both lower case and upper case, and should be at least 8 characters.");
+        validator.check = function(val) {
+            return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[0-9a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,}$/.test(val);
+        }
     }
     if (name == 'email') {
-
+        validator.msg.set('invalid', "Your email address is invalid..");
+        validator.check = function(val) {
+            return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val);
+        }
     }
     if (name == 'phone') {
-
+        validator.msg.set('invalid', "Your phone number is not correct..");
+        validator.check = function(val) {
+            return /^\d+$/.test(val);
+        }
     }
     return validator;
 }
@@ -76,8 +102,13 @@ function prepareDOMnodeFor(style, $generatedForm, formlist) {
         innerHTMLtoAppend += '<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">';
         innerHTMLtoAppend += '<input class="mdl-textfield__input" type="' + formlist.type + '" id="' + formlist.label + '">';
         innerHTMLtoAppend += '<label class="mdl-textfield__label" for="' + formlist.label + '">' + formlist.label + '</label>';
-        innerHTMLtoAppend += '<span class="mdl-textfield__error" id="msgFor' + formlist.label + '"></span></div>'
-        $generatedForm.html($generatedForm.html() + innerHTMLtoAppend);
+        innerHTMLtoAppend += '<span class="mdl-textfield__error" id="msgFor' + formlist.label + '" style="color:red"></span></div>'
+        $generatedForm.append(innerHTMLtoAppend);
+    } else {
+        innerHTMLtoAppend += '<label for="' + formlist.label + '">' + formlist.label + '</label><br>';
+        innerHTMLtoAppend += '<input type="' + formlist.type + '" id="' + formlist.label + '"><br>';
+        innerHTMLtoAppend += '<span id="msgFor' + formlist.label + '" style = "color:red"></span></div><br>'
+        $generatedForm.append(innerHTMLtoAppend);
     }
     var $newNode = $("#" + formlist.label);
     var $msg = $("#msgFor" + formlist.label);
@@ -85,9 +116,7 @@ function prepareDOMnodeFor(style, $generatedForm, formlist) {
 }
 
 function setUpEventListener($newNode, $msg, formlist) {
-    console.log($newNode);
     $newNode.keyup(function() {
-        console.log($newNode);
         if (formlist.validator.check($newNode.val())) { // the input value is valid
             $msg.html(formlist.validator.msg.get('success'));
             $msg.css('visibility', 'visible');
